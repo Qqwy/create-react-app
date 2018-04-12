f5cfb4d9c6729f77c7aaa43939eef575a0211a73
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-04-12 14:49:32 +0200

    Sounds support in webpack

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -167,6 +167,15 @@ module.exports = {
               name: 'static/media/[name].[hash:8].[ext]',
             },
           },
+            {
+                // AUDIO FILES
+                test: [/\.mp3$/, /\.ogg$/, /\.webm/, /\.flac/],
+                loader: require.resolve('url-loader'),
+                options: {
+                    limit: 10000,
+                    name: 'static/media/[name].[hash:8].[ext]',
+                },
+            },
           // Process JS with Babel.
           {
             test: /\.(js|jsx|mjs)$/,
@@ -249,7 +258,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.mp3$/, /\.ogg$/, /\.webm/, /\.flac/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
             },
